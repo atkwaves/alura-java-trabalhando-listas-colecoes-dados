a1857ea4d339c087a5d8158eb7e8042a7eb71b49
Alvaro Kieling <atkwaves@gmail.com>
2024-07-16 20:55:54 -0300

    A01 Coleção de filmes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# alura-java-trabalhando-listas-colecoes-dados
+<h1>
+    <a href="https://cursos.alura.com.br/course/java-criando-primeira-aplicacao">
+     <img align="center" width="64px" src="https://www.alura.com.br/assets/api/cursos/java-listas-colecoes-dados.svg">
+    </a>
+    <span>
+        Curso de Java: Trabalhando com listas e coleções de dados
+    </span>
+</h1>
+
+## 🎯 Objetivos
+
+- Aprenda a representar uma lista de objetos em Java;
+- Entenda o funcionamento da classe ArrayList do Java;
+- Manipule uma lista de objetos;
+- Realize ordenação de uma lista de acordo com algum critério;
+- Faça casting de objetos e verificação de tipo com instanceof.
+
+## 📚 Aulas
+
+**A01** Coleção de filmes
+
+**A02** Construindo objetos
+
+**A03** Lista de objetos distintos
+
+**A04** Ordenando dados
+
+**A05** Consolidando conhecimentos
+
+## 🔧 Ferramentas
+
+[![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=openjdk&logoColor=white)](https://docs.oracle.com/en/java/)
---
+++ screenmatch/src/Principal.java
@@ -0,0 +1,67 @@
+import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
+import java.util.ArrayList;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Filme favorito = new Filme();
+        favorito.setNome("The Matrix");
+        favorito.setAnoDeLancamento(1999);
+        favorito.setDuracaoEmMinutos(135);
+        favorito.setIncluidoNoPlano(true);
+
+        Filme outro = new Filme();
+        outro.setNome("John Wick");
+        outro.setAnoDeLancamento(2014);
+        outro.setDuracaoEmMinutos(101);
+        outro.setIncluidoNoPlano(true);
+
+        Serie serie = new Serie();
+        serie.setNome("La Casa de Papel");
+        serie.setAnoDeLancamento(2017);
+        serie.setIncluidoNoPlano(true);
+        serie.setAtiva(true);
+        serie.setTemporadas(5);
+        serie.setEpisodiosPorTemporada(10);
+        serie.setMinutosPorEpisodio(45);
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(favorito);
+        calculadora.inclui(outro);
+        calculadora.inclui(serie);
+
+        System.out.println("Tempo total: " + calculadora.getTempoTotal());
+
+        Episodio primeiro = new Episodio();
+        primeiro.setNumero(1);
+        primeiro.setSerie(serie);
+        primeiro.setTotalVisualizacoes(300);
+
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(favorito);
+        filtro.filtra(outro);
+        filtro.filtra(primeiro);
+
+        var filmeDoPaulo = new Filme();
+        filmeDoPaulo.setNome("Dogville");
+        filmeDoPaulo.setAnoDeLancamento(2003);
+        filmeDoPaulo.setDuracaoEmMinutos(200);
+        filmeDoPaulo.avalia(10);
+
+        ArrayList<Filme> listaDeFilmes = new ArrayList<>();
+        listaDeFilmes.add(filmeDoPaulo);
+        listaDeFilmes.add(favorito);
+        listaDeFilmes.add(outro);
+
+        System.out.println("Tamanho da lista: " + listaDeFilmes.size());
+        System.out.println("Primeiro filme: " + listaDeFilmes.get(0).getNome());
+        System.out.println(listaDeFilmes);
+        System.out.println("toString filme: " + listaDeFilmes.get(0).toString());
+    }
+}
